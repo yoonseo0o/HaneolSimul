ca2f36b7f943938a443d4ecba8e9f68ec151a8ae
yoonseo0o <muddy0618@naver.com>
2024-05-21 06:07:12 +0900

    CurtainPuzzle

---
+++ PuddingThief/Assets/Scripts/CurtainPuzzle.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurtainPuzzle : MonoBehaviour
+{
+    [SerializeField] Button[] curtain_button;
+    [SerializeField] GameObject[] curtain_obj;
+
+    // 시작시 각 버튼의 onclick에 함수를 입력함.
+    private void Awake()
+    {
+        for (int i = 0; i < curtain_button.Length; i++)
+        {
+            int index = i;
+            curtain_button[i].onClick.AddListener(() => OnClick(index));
+        }
+    }
+    public void OnClick(int index)
+    {
+        // 커튼 올라가면 모든 버튼 비활성화 시키고 
+        for(int i=0;i<curtain_button.Length;i++)
+            curtain_button[i].interactable = false;
+
+        StartCoroutine(Print_Result(index));
+    }
+    IEnumerator Print_Result(int index)
+    {
+        // n초 후 결과 출력 
+        yield return new WaitForSeconds(1);
+        curtain_obj[index].transform.GetChild(1).gameObject.SetActive(true);
+
+        // 게임 오버, 다음 스테이지 이동 결정
+        if (index == 0)
+            Next_Stage();
+        else
+            Game_Over();
+
+    }
+    // 임시 스테이지 변경 함수
+    void Next_Stage()
+    {
+        Debug.Log("Go_NextStage");
+    }
+    // 임시 게임 오버 함수
+    void Game_Over()
+    {
+        Debug.Log("GameOver");
+
+    }
+}
